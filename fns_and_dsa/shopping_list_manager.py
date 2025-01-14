f5d1c0ed5af57e5915c64e7926861a05e02642a5
Ahmed <ahmedsquare@aucegypt.edu>
2025-01-14 23:51:56 +0200

    Add Shopping List file

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,7 +12,7 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item  = input("Please input the item you want to add to the list: ")
+            item  = input("Enter the item to add: ")
             shopping_list.append(item)
         elif choice == '2':
             item  = input("Please input the item you want to remove from the list: ")
