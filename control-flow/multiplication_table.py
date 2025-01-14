29940119c351671a6c4e339fdd4cbc4821bed614
Ahmed <ahmedsquare@aucegypt.edu>
2025-01-14 03:30:49 +0200

    ADD multiplication_table file

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
-number =  input("Enter a number to see its multiplication table:")
+number =  int(input("Enter a number to see its multiplication table:"))
 
 for Y in range(1,11):
     Z = number * Y
