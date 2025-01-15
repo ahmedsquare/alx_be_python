6aaa94dd51daf444a6efb3b8215aff4b360862f4
Ahmed <ahmedsquare@aucegypt.edu>
2025-01-15 09:20:13 +0200

    Update Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,13 +1,13 @@
 class BankAccount:
-    def __init__(self, balance: float = 0):
+    def __init__(self, balance = 0):
         self.account_balance = balance
 
-    def deposit(self, amount: float)->None:
+    def deposit(self, amount)->None:
         self.account_balance += amount
         print(f"Deposited: ${amount}")
 
 
-    def withdraw(self, amount: float)->None:
+    def withdraw(self, amount)->None:
         if(amount > self.account_balance):
             print("Insufficient funds.")
         else:
