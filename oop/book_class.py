2d44898a20f3427ae0ac0a7142be63163891eb97
Ahmed <ahmedsquare@aucegypt.edu>
2025-01-21 22:13:07 +0200

    ADDED BOOK Class

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,8 +1,8 @@
 class Book:
     def __init__(self, title, author, year):
-        _title_ = title
-        _author_ = author
-        _year_ = year
+        self.title = title
+        self.author = author
+        self.year = year
     
     def __del__(self):
         print(f"Deleting {self.title}")
