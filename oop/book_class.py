2f0cc45e4ac1f435de7211d37bc1cb666f14fe6b
Ahmed <ahmedsquare@aucegypt.edu>
2025-01-21 22:11:24 +0200

    ADDED BOOK Class

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,14 +1,14 @@
 class Book:
     def __init__(self, title, author, year):
-        __title__ = title
-        __author__ = author
-        __year__ = year
+        _title_ = title
+        _author_ = author
+        _year_ = year
     
     def __del__(self):
-        print(f"Deleting {self.__title__}")
+        print(f"Deleting {self._title_}")
 
     def __str__(self):
-        return f"{self.__title__} by {self.__author__} published in {self.__year__}"
+        return f"{self._title_} by {self._author_} published in {self._year_}"
     
     def __repr__(self):
-        return f"Book('{self.title}', '{self.author}', {self.year})"
+        return f"Book('{self._title_}', '{self._author_}', {self._year_})"
