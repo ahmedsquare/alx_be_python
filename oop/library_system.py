c364b21b43e4d9ee69a38c6d3776c84bf0932c00
Ahmed <ahmedsquare@aucegypt.edu>
2025-01-22 02:53:19 +0200

    Updated Library class

--- oop/library_system.py
+++ oop/library_system.py
@@ -6,10 +6,10 @@ class Book:
         print(f"Deleting {self.title}")
 
     def __str__(self):
-        return f"{self.title} by {self.author}, published in {self.year}"
+        return f"{self.title} by {self.author}"
     
     def __repr__(self):
-        return f"Book('{self.title}', '{self.author}', {self.year})"
+        return f"Book('{self.title}', '{self.author}'"
     
 class EBook(Book):
     def __init__(self, title, author,file_size):
