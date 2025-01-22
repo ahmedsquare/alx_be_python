d91724c8c77644b8361b66ccd10f271860f1a41a
Ahmed <ahmedsquare@aucegypt.edu>
2025-01-22 02:59:52 +0200

    Updated Library class

--- oop/library_system.py
+++ oop/library_system.py
@@ -16,7 +16,7 @@ class EBook(Book):
         super().__init__(title,author)
         self.file_size = file_size
     def __str__(self):
-        return f"{super().__str__}, File Size: {self.file_size}"
+        return f"{super().__str__()}, File Size: {self.file_size}"
 
 
 class PrintBook(Book):
@@ -24,7 +24,7 @@ class PrintBook(Book):
         super().__init__(title,author)
         self.page_count = page_count
     def __str__(self):
-        return f"{super().__str__}, Page Count: {self.page_count}"
+        return f"{super().__str__()}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self):
@@ -35,4 +35,4 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(book)
+            print(f"{book.__class__.__name__}: {book}")
